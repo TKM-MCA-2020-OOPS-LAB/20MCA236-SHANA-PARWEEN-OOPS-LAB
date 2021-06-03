78f0616c14c3ea98a8e38e19670401d30d8a2cf2
SHANA2029 <75963258+SHANA2029@users.noreply.github.com>
2021-06-03 21:48:44 +0530

    COMMIT

---
+++ C04/Q1/Main.java
@@ -0,0 +1,12 @@
+import graphics.area;
+public class Main {
+
+	public static void main(String[] args) 
+	{
+		area ar=new area();
+		ar.rectangle();
+		ar.triangle();
+		ar.square();
+		ar.circle();
+	}
+}
