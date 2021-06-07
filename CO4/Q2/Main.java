cfa800fbf72170c46482c24bf55c6dfda1b6d242
SHANA2029 <75963258+SHANA2029@users.noreply.github.com>
2021-06-07 20:05:15 +0530

    COMMIT

---
+++ CO4/Q2/Main.java
@@ -0,0 +1,14 @@
+import Arithmetic.Math;
+public class Main {
+	public static void main(String[] args) 
+	{
+
+		Math obj = new Math();
+		
+		System.out.println("   ARITHMETIC OPERATIONS   ");
+    	obj.addition();
+    	obj.subtraction();
+    	obj.multiplication();
+    	obj.division();
+	}
+}
