a56182c03b5edaa36edf121d646d46e3393439bb
SHANA2029 <75963258+SHANA2029@users.noreply.github.com>
2021-08-06 20:14:20 +0530

    COMMIT

---
+++ CO4/Q11/LL_remove.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class LL_remove {
+	public static void main(String[] args) {
+	     LinkedList<String> l_list = new LinkedList<String>();
+	          l_list.add("Shana");
+	          l_list.add("Sahala");
+	          l_list.add("Anjana");
+	          l_list.add("Amrutha");
+	          l_list.add("Anila");
+	     
+	   System.out.println("The linked list is: " + l_list);
+
+	    l_list.clear();
+	 
+	    System.out.println("The  linked list after removal of elements : " + l_list);
+	}
+
+}
